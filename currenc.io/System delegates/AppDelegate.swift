5700cea5d6f7eced728faa2a9c7e216d75aa2cb6
Ostap <admin@Ostaps-MacBook-Air.local>
2020-04-02 13:44:49 +0300

    Initial commit

---
+++ currenc.io/System delegates/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  currenc.io
+//
+//  Created by Ostap on 02.04.2020.
+//  Copyright © 2020 Ostap Tyvonovych. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
---
+++ currenc.io/System delegates/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  currenc.io
+//
+//  Created by Ostap on 02.04.2020.
+//  Copyright © 2020 Ostap Tyvonovych. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+}
+
